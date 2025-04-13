ff82b8001574a63711be3049826f05ebf5b6619c
Bolat Abdulaziz <Bola_cg56@student.itstep.kz>
2025-04-13 18:09:10 +0500

    feat: Added requests to login and register

--- js/login.js
+++ js/login.js
@@ -1,26 +1,63 @@
+async function login(email, password) {
+	try {
+	const response = await fetch('http://localhost:8080/api/auth/login', {
+		method: 'POST',
+		headers: {
+		'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ email, password }),
+	});
+
+	if (!response.ok) {
+		throw new Error('Неверный логин или пароль');
+	}
+
+	const data = await response.json();
+	const token = data.token;
+
+	localStorage.setItem('jwt', token);
+
+	window.location.href = 'index.html';
+	} catch (err) {
+	console.error('Ошибка входа:', err.message);
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	const form = document.getElementById("login-form");
+
+	form.addEventListener("submit", function (e) {
+		e.preventDefault();
+
+		const email = document.getElementById("email").value;
+		const password = document.getElementById("password").value;
+
+		login(email, password);
+	});
+});
+
+
 (function($) {
 
 	"use strict";
 
 	var fullHeight = function() {
-
 		$('.js-fullheight').css('height', $(window).height());
 		$(window).resize(function(){
 			$('.js-fullheight').css('height', $(window).height());
 		});
-
 	};
 	fullHeight();
 
 	$(".toggle-password").click(function() {
-
-	  $(this).toggleClass("fa-eye fa-eye-slash");
-	  var input = $($(this).attr("toggle"));
-	  if (input.attr("type") == "password") {
-	    input.attr("type", "text");
-	  } else {
-	    input.attr("type", "password");
-	  }
+		$(this).toggleClass("fa-eye fa-eye-slash");
+		var input = $($(this).attr("toggle"));
+		if (input.attr("type") == "password") {
+			input.attr("type", "text");
+		} else {
+			input.attr("type", "password");
+		}
 	});
 
+	
 })(jQuery);
--- js/register.js
+++ js/register.js
@@ -1,3 +1,54 @@
+async function register(username, city, email, password) {
+	try {
+		const response = await fetch('http://localhost:8080/api/auth/register', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ username, city, email, password }),
+		});
+
+		if (!response.ok) {
+			throw new Error('Неверные данные');
+		}
+
+		alert('Регистрация прошла успешно!');
+		window.location.href = 'login.html';
+	} catch (err) {
+		console.error('Ошибка регистрации:', err.message);
+		alert('Ошибка регистрации: ' + err.message);
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	const form = document.getElementById("register-form");
+
+	form.addEventListener("submit", function (e) {
+		e.preventDefault();
+
+		const username = document.getElementById("username").value;
+		const city = document.getElementById("city").value;
+		const email = document.getElementById("email").value;
+		const password = document.getElementById("password").value;
+		const confirmPassword = document.getElementById("confirm-password").value;
+
+		console.log(username, city, email, password);
+
+		if (!username || !city || !email || !password || !confirmPassword) {
+			alert("Пожалуйста, заполните все поля");
+			return;
+		}
+
+		if (password === confirmPassword) {
+			register(username, city, email, password);
+		} else {
+			alert("Пароли не совпадают");
+		}
+	});
+});
+
+
+
 (function($) {
 
 	"use strict";
